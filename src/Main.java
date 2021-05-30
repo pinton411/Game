6fa847569315bdfdc11c3eff5a8f57b9477c31d0
Michael Neiman <pinton411@gmail.com>
2021-05-30 14:29:48 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My name is: Michael Neiman");
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame();
+            }
+        });
     }
 }
